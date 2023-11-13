6b7fad1b9665c25d3f4660e7bcf61dc81f165494
Omkar-ctrl <72722881+Omkar-ctrl@users.noreply.github.com>
2023-11-13 09:06:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+var csr= document.querySelector("#cursor");
+var blur= document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove",function(dets){
+
+    csr.style.left= dets.x+"px";
+    csr.style.top= dets.y+"px";
+    blur.style.left= dets.x-250+"px";
+    blur.style.top= dets.y-250+"px";
+})
+
+
+
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "120px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        // markers:true,
+        start:" top -10%",
+        end: "top -11%",
+        scrub:2
+    }
+    
+  });
+
+
+gsap.to("#main",{
+    backgroundColor: "#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        // markers :true,
+        start: " top -50%",
+        end: "top -80%",
+        scrub : 2
+    }
+
+})
